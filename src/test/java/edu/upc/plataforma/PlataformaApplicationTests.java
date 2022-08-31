486fec948922d38662afd5cfbd6d48c7c4e1c3bf
francisco <fjariasvilela@gmail.com>
2022-08-30 23:00:20 -0500

    initial commit

---
+++ src/test/java/edu/upc/plataforma/PlataformaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upc.plataforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
